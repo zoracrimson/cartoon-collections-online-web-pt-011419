00bf92dc65b0c201e4d73306c14fdbe58c1d203d
Kat Maldon <katharinamaldon@gmail.com>
2019-04-03 15:15:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,12 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  if 
-    calls_long.length > 4
-    true
+  if calls_long.length > 4
+    true 
   else
-    false
+    false 
   end 
-end
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
